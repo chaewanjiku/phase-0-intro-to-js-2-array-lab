2cf9f0c354008774e7e8bf0fb4b72ed8f9c1ff3d
Charity <charitywanjiku8245@gmail.com>
2024-03-25 22:38:55 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(){
+    cats.push('Ralph');
+ }
+ function destructivelyPrependCat(){
+    cats.unshift('Bob');
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+ }
+ function appendCat(name){
+    return [...cats,name]
+ }
+function prependCat(name){
+    let copyCats = [name,...cats]
+    return copyCats;
+}
+function removeLastCat(){
+    let remainderCats = cats.slice(0,2);
+    return remainderCats;
+}
+function removeFirstCat(){
+    let remainderCats = cats.slice(1);
+    return remainderCats;
+}
